bcf301cf2ecfb5415f569c50064efbfbcd165388
Xin Zheng <zhengxin@mit.edu>
2017-01-24 16:24:24 -0500

    1

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 const indexPath = path.join(__dirname, '/public/index.html');
 const publicPath = express.static(path.join(__dirname, '/public'));
 
-app.use('/public', publicPath);
+app.use(publicPath);
 app.get('/', function (_, res) { res.sendFile(indexPath) });
 
 
+app.listen(port);
