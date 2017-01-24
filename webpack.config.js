230a1f8b857169c636fd1c235abc2a025f94510e
Xin Zheng <zhengxin@mit.edu>
2017-01-24 14:45:45 -0500

    remove react hot

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
         exclude: /node_modules/,
       },
       {
