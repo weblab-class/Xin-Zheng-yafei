32f9f04d7f59cd7c5ab5a5a211b4ff3786958b66
Xin Zheng <zhengxin@mit.edu>
2017-01-24 16:27:47 -0500

    2

--- server.js
+++ server.js
@@ -5,13 +5,28 @@
 const path = require('path');
 const express = require('express');
 
+//
+// const app = express();
+// const indexPath = path.join(__dirname, '/public/index.html');
+// const publicPath = express.static(path.join(__dirname, '/public'));
+//
+// app.use(publicPath);
+// app.get('/', function (_, res) { res.sendFile(indexPath) });
+//
+
 
 const app = express();
-const indexPath = path.join(__dirname, '/public/index.html');
-const publicPath = express.static(path.join(__dirname, '/public'));
+const port = process.env.PORT || 8080;
+const public_path = express.static(__dirname + '/public');
+const index_path = __dirname + '/public/index.html';
 
-app.use(publicPath);
-app.get('/', function (_, res) { res.sendFile(indexPath) });
-
-
+app.use(public_path);
+app.get('*', function (request, response) {
+  response.sendFile(index_path, function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
 app.listen(port);
+console.log("Listening at http://localhost:" + port);
