a6e8924aeba7cf47becf7a3262a38741a3d20c90
DeborahHan <yafei@mit.edu>
2017-01-28 15:43:53 -0500

    calendar UI change

--- src/components/CalendarInfo/CalendarInfo.js
+++ src/components/CalendarInfo/CalendarInfo.js
@@ -596,7 +596,9 @@ class CalendarInfo extends React.Component {
         <div className={s.appWrap}>
 
           <div className={s.areaSubmit}>
-            <button className={s.buttonSubmit} onClick={this.handleSubmit}><span>Submit </span></button>
+            <button className={s.buttonSignOut} onClick={this.handleSubmit}><i className="fa fa-sign-out  fa-2.5x"/></button>
+            <h1>Select Event</h1>
+            <button className={s.buttonSubmit} onClick={this.handleSubmit}><i className="fa fa-paper-plane  fa-2.5x"/></button>
           </div>
 
           <div className={s.areaContent}>
@@ -609,13 +611,11 @@ class CalendarInfo extends React.Component {
                                   onClickAll={this.onClickAll} pressDelay={200}/>
           </div>
 
-
-
           <div className={s.areaOption}>
-            <button className={s.buttonCalOpt}><i className="fa fa-newspaper-o" onClick={() => this.changePanel("Title")}/>Event</button>
-            <button className={s.buttonCalOpt}><i className="fa fa-clock-o" onClick={() => this.changePanel("Time")}/>Time</button>
-            <button className={s.buttonCalOpt}><i className="fa fa-map-marker" onClick={() => this.changePanel("Place")}/>Place</button>
-            <button className={s.buttonCalOpt}><i className="fa fa-link" onClick={() => this.changePanel("URL")}/>URL</button>
+            <button className={s.buttonCalOpt}><i className="fa fa-newspaper-o" onClick={() => this.changePanel("Title")}/></button>
+            <button className={s.buttonCalOpt}><i className="fa fa-clock-o" onClick={() => this.changePanel("Time")}/></button>
+            <button className={s.buttonCalOpt}><i className="fa fa-map-marker" onClick={() => this.changePanel("Place")}/></button>
+            <button className={s.buttonCalOpt}><i className="fa fa-link" onClick={() => this.changePanel("URL")}/></button>
 
           </div>
         </div>
